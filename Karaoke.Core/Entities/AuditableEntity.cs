617ec2ba75db9c2134b96bf2c66ff78cdef257b4
Florian Theron <mikyan0207@gmail.com>
2023-05-28 16:09:02 +0200

    Add AuditableEntity

---
+++ Karaoke.Core/Entities/AuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Karaoke.Core.Entities;
+
+public class AuditableEntity
+{
+    public DateTime Created { get; set; }
+
+    public Guid CreatedBy { get; set; } = Guid.Empty;
+
+    public DateTime? LastModified { get; set; }
+
+    public Guid LastModifiedBy { get; set; } = Guid.Empty;
+}
