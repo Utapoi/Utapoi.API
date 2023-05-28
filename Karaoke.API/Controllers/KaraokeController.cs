185a2ebd308819c7df06a4fc4b902445352f4a5f
Florian Theron <mikyan0207@gmail.com>
2023-05-28 14:11:05 +0200

    Setup API Solution

---
+++ Karaoke.API/Controllers/KaraokeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Karaoke.API.Controllers;
+
+/// <summary>
+///     The base controller for the application.
+/// </summary>
+[ApiController]
+[Route("[controller]")]
+public class KaraokeController : Controller
+{
+}
