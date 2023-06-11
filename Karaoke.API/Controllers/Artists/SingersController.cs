d6d319e9df9333d902ede7d6919dbf00206af3d2
Florian Theron <mikyan0207@gmail.com>
2023-06-11 10:52:03 +0200

    KA-3: Add GET /Singers Endpoint with Pagination (#5)

--- Karaoke.API/Controllers/Artists/SingersController.cs
+++ Karaoke.API/Controllers/Artists/SingersController.cs
@@ -15,6 +15,19 @@ namespace Karaoke.API.Controllers.Artists;
 [Authorize(Roles = "User")]
 public class SingersController : ApiControllerBase
 {
+    private readonly ILogger<SingersController> _logger;
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="SingersController" /> class.
+    /// </summary>
+    /// <param name="logger">
+    ///     The <see cref="ILogger{TCategoryName}" />.
+    /// </param>
+    public SingersController(ILogger<SingersController> logger)
+    {
+        _logger = logger;
+    }
+
     /// <summary>
     ///     Gets a list of singers.
     /// </summary>
@@ -28,22 +41,25 @@ public class SingersController : ApiControllerBase
     [Authorize(Roles = "User")]
     [ProducesResponseType(typeof(PaginatedResponse<SingerDTO>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> GetSingersAsync([FromQuery] PaginatedRequest request)
     {
-        var m = new GetSingers.Request
+        try
         {
-            Skip = request.Skip,
-            Take = request.Take
-        };
+            var result = await Mediator.Send(new GetSingers.Request(request.Skip, request.Take));
 
-        var result = await Mediator.Send(m);
+            if (result.IsFailed)
+            {
+                return BadRequest();
+            }
 
-        if (result.IsFailed)
-        {
-            return BadRequest();
+            return Ok(result.Value);
         }
-
-        return Ok(result.Value);
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error getting singers.");
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
     }
 
     /// <summary>
--- Karaoke.Application/Singers/Requests/GetSingers/GetSingers.cs
+++ Karaoke.Application/Singers/Requests/GetSingers/GetSingers.cs
@@ -2,48 +2,78 @@
 using FluentResults;
 using Karaoke.Application.Common;
 using Karaoke.Application.DTO;
-using Karaoke.Application.Persistence;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 
 namespace Karaoke.Application.Singers.Requests.GetSingers;
 
+/// <summary>
+///     Represents a request to get singers.
+/// </summary>
 public static class GetSingers
 {
-    public sealed record Request : IRequest<Result<PaginatedResponse<SingerDTO>>>
+    /// <summary>
+    ///     Represents a request to get a paginated list of singers.
+    /// </summary>
+    public sealed class Request : IRequest<Result<PaginatedResponse<SingerDTO>>>
     {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Request" /> class.
+        /// </summary>
+        /// <param name="skip">The number of items to skip.</param>
+        /// <param name="take">The number of items to take.</param>
+        public Request(int skip, int take)
+        {
+            Skip = skip;
+            Take = take;
+        }
+
+        public Request()
+        {
+        }
+
+        /// <summary>
+        ///     Gets the number of items to skip.
+        /// </summary>
         public int Skip { get; init; }
 
+        /// <summary>
+        ///     Gets the number of items to take.
+        /// </summary>
         public int Take { get; init; }
     }
 
+    /// <summary>
+    ///     Represents a handler for <see cref="Request" />.
+    /// </summary>
     internal sealed class Handler : IRequestHandler<Request, Result<PaginatedResponse<SingerDTO>>>
     {
-        private readonly IKaraokeDbContext _context;
-
         private readonly IMapper _mapper;
 
-        public Handler(IKaraokeDbContext context, IMapper mapper)
+        private readonly ISingersService _singersService;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Handler" /> class.
+        /// </summary>
+        /// <param name="singersService">The <see cref="ISingersService" />.</param>
+        /// <param name="mapper">The <see cref="IMapper" />.</param>
+        public Handler(ISingersService singersService, IMapper mapper)
         {
-            _context = context;
+            _singersService = singersService;
             _mapper = mapper;
         }
 
-        public async Task<Result<PaginatedResponse<SingerDTO>>> Handle(Request request,
-            CancellationToken cancellationToken)
+        public async Task<Result<PaginatedResponse<SingerDTO>>> Handle(
+            Request request,
+            CancellationToken cancellationToken
+        )
         {
-            var singers = await _context.Singers
-                .Include(x => x.Names)
-                .Include(x => x.ProfilePicture)
-                .Skip(request.Skip)
-                .Take(request.Take)
-                .ToListAsync(cancellationToken);
+            var singers = await _singersService.GetAsync(request, cancellationToken);
 
             return Result.Ok(new PaginatedResponse<SingerDTO>
             {
                 Items = _mapper.Map<IEnumerable<SingerDTO>>(singers),
                 Count = singers.Count,
-                TotalCount = await _context.Singers.CountAsync(cancellationToken)
+                TotalCount = await _singersService.CountAsync(cancellationToken)
             });
         }
     }
